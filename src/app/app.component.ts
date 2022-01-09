b5218022cf1654206da29a7f9b22f8fd36f2ad2f
Abhii27 <abhishekpanchal@1998>
2022-01-09 20:16:45 +0530

    Typing Game added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+//lorem.sentence()
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TypeGame';
+
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(event: any) {
+    //console.log(event.target.value);
+    this.enteredText = event.target.value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if(!enteredLetter){
+      return 'pending';
+    }
+    
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
+
 }
